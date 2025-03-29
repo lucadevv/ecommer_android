8bfc737ac159d4fe389e66ea783d7a186c4a69ae
Luis Carranza <luiscodecarranza@gmail.com>
2025-03-29 15:54:28 -0500

    firts-commit

---
+++ app/src/main/java/com/example/shopping_app/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.shopping_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val navController = rememberNavController()
+
+            App()
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/shopping_app/app.kt
@@ -0,0 +1,12 @@
+package com.example.shopping_app
+
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun App(
+
+){
+
+
+}
